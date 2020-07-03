21a3eecc83aded2f4db6d1d90f17e989ed42e1a5
shahroz461 <kshahroz850@gmail.com>
2020-07-03 16:33:08 +0500

    table print modiified calculator

--- calc.py
+++ calc.py
@@ -1,14 +1,16 @@
 print("for add press 1")
 print("for sub press 2")
+print("for multiplication press 3")
+print("for division press 4")
 '''user input leygay hum is variable '''
 print("select your menu: ")
+
 username = input()
 
 print("Enter First value: ")
 a = int(input())
 print("Enter Second Value: ")
 b = int(input())
-
 if username=="1":
     print("Addition")
     c = a + b
@@ -18,9 +20,19 @@ elif username=="2":
     print("Subtraction")
     d = a - b
     print(d)
+    
+elif username=="3":
+    print("multiplication")
+    e = a * b
+    print(e)
+elif username=="4":
+    print("division")
+    f = a / b
+    print(f)
 
 else:
 
     print("error: Select vslid input")
+
 
 
---
+++ table.py
@@ -0,0 +1,10 @@
+i = 1
+print("Enter varaible to print table: ")
+t = int(input(''))
+while i < 10:
+ 
+  i = i + 1
+  print(str(t) + " x "+str(i) + " = " + str(i*t))
+
+
+#for loop
